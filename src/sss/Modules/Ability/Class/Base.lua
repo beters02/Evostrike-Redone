b3c529c7d2b50ca18d95896981e82d6eb263e229
Bryce Peters <91118447+beters02@users.noreply.github.com>
2023-07-29 00:10:39 -0600

    ability updates

--- src/sss/Modules/Ability/Class/Base.lua
+++ src/sss/Modules/Ability/Class/Base.lua
@@ -1,16 +1,43 @@
 --[[ Base AbilityClass ]]
 
 local Base = {}
+Base.__index = Base
 
 function Base.new(abilityClassModule)
     if not abilityClassModule then warn("Must specify AbilityClassModule") return end
-
     local self = setmetatable(require(abilityClassModule), Base)
     return self
 end
 
-function Base:AttemptUse()
+function Base:ServerUseVarCheck()
+    local useSuccess, err = self.remoteFunction:InvokeServer("CanUse")
+    if not useSuccess then
+        return false, warn("ABILITY USE ERROR, " .. err)
+    end
+
+    local newUses = useSuccess
+    self.uses = newUses
+    print(self.uses)
+    return self.uses
+end
+
+function Base:StartClientCooldown()
+    self.cooldown = true
+    task.delay(0.07, function()
+        for i = self.cooldownLength, 0, -1 do
+            if i == 0 then
+                self.cooldown = false
+                self.frame.Key.Text = self.key
+                break
+            end
+            self.frame.Key.Text = tostring(i)
+            task.wait(1)
+        end
+    end)
     
+    --[[task.delay(0.02 + self.cooldownLength, function()
+        self.cooldown = false
+    end)]]
 end
 
 return Base
--- src/sss/Modules/Ability/Scripts/AbilityServer.server.lua
+++ src/sss/Modules/Ability/Scripts/AbilityServer.server.lua
@@ -2,7 +2,7 @@ local abilityName = string.gsub(script.Parent.Parent.Name, "AbilityFolder_", "")
 local abilityOptions = require(game:GetService("ServerScriptService"):WaitForChild("Modules"):WaitForChild("Ability").Class[abilityName])
 local abilityRemoteEvent = script.Parent.Parent.Remotes.AbilityRemoteEvent
 local abilityRemoteFunction = script.Parent.Parent.Remotes.AbilityRemoteFunction
-local serverStoredVar = {uses = abilityOptions.uses}
+local serverStoredVar = {uses = abilityOptions.uses, cooldown = false}
 
 local Functions = {}
 
@@ -13,6 +13,7 @@ Functions.Timer = function(timerType)
 	if not length then error("Could not find timer " .. tostring(timerType)) end
 	endTime += length
 	repeat task.wait() until tick() >= endTime
+	print('server cooldown finisehd')
 	return true
 end
 
@@ -20,9 +21,17 @@ Functions.GetVar = function()
     return serverStoredVar
 end
 
-Functions.SubUses = function()
-    serverStoredVar.uses -= 1
-    return serverStoredVar.uses
+Functions.CanUse = function()
+	if not serverStoredVar.cooldown then
+		if serverStoredVar.uses <= 0 then
+			return false, "USES MISMATCH"
+		end
+		serverStoredVar.uses -= 1
+		Functions.Timer("Cooldown")
+		return serverStoredVar.uses
+	end
+    
+    return false, "COOLDOWN MISMATCH"
 end
 
 abilityRemoteFunction.OnServerInvoke = function(player, action, ...)
