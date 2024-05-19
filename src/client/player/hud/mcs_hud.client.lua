eb6629a34e7fd68691154c6b44c6ac47370d4ea5
Bryce Peters <91118447+beters02@users.noreply.github.com>
2024-05-18 19:49:39 -0600

    prevent potential hud memory leak

--- src/client/player/hud/mcs_hud.client.lua
+++ src/client/player/hud/mcs_hud.client.lua
@@ -1,24 +1,32 @@
 local player = game:GetService("Players").LocalPlayer
 if not player:GetAttribute("Loaded") then repeat task.wait() until player:GetAttribute("Loaded") end
+
+local didInit = false
 local _ = player.Character or player.CharacterAdded:Wait()
 local hud = require(script.Parent)
-
 hud = hud.initialize(player)
 
-player.CharacterAdded:Connect(function(char)
+-- update version from workspace
+local version = workspace:GetAttribute("Version")
+hud.gui:WaitForChild("Version").Text = "Evostrike Version " .. tostring(version)
+
+local function initCharHud(char)
+    if not didInit then
+        didInit = true
+    end
     hud:ConnectPlayer()
 
     local hum = char:WaitForChild("Humanoid")
     hum.Died:Once(function()
         hud:DisconnectPlayer()
     end)
-end)
-
-if player.Character then
-    hud:ConnectPlayer()
-
-    local hum = player.Character:WaitForChild("Humanoid")
-    hum.Died:Once(function()
-        hud:DisconnectPlayer()
-    end)
 end
+
+local function start()
+    player.CharacterAdded:Connect(initCharHud)
+    if player.Character and not didInit then
+        initCharHud(player.Character)
+    end
+end
+
+start()
